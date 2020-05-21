d6b111517df719554d0232cd9158a96f24bf8095
Joseph Kelleher <joekelleher94@gmail.com>
2020-05-20 22:13:12 -0400

    Navbar created and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css'
+import { Route, Switch, Link } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo right">Water My Plants</a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li className="active"><Link to="/">Home</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Sign Up</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="to-do">
+            <h3>The project begins</h3>
+          </div>
+        </div>
     </div>
   );
 }
