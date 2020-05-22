dec5764740c982ee2509151426f6ed1564606098
c00kamunga <tsothalanti2@yahoo.com>
2020-05-21 17:16:13 -0700

    imported reducer and Provider into index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reducer from './reducer'
 
-
+import { Provider } from 'react-redux'
 import { createStore } from "redux"
 
 <<<<<<< HEAD
