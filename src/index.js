bc498912ca8e735f62aa59e8449a28ed9b0e1c3c
c00kamunga <tsothalanti2@yahoo.com>
2020-05-21 17:13:20 -0700

    created store within index.js and created reducer.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
+import { createStore } from "redux"
+
 <<<<<<< HEAD
 
 =======
 import { BrowserRouter as Router } from 'react-router-dom';
 >>>>>>> 2f90c3d5a41425526813bf54401fc46c46295e19
+
+
+
+const store = createStore(reducer)
+
 
 ReactDOM.render(
   <React.StrictMode>
