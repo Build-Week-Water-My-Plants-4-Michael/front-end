4c80683df9b004b1341f5b8dadf8183fb5289096
c00kamunga <tsothalanti2@yahoo.com>
2020-05-22 19:28:42 -0700

    typed createUser function with post request for creating new account

--- src/actions.js
+++ src/actions.js
@@ -1,29 +1,52 @@
-export const CREATE_USER_START = 'CREATE_USER_START'
-export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
-export const CREATE_USER_FAILED = 'CREATE_USER_FAILED'
+import { axiosWithAuth } from "./axiosWithAuth/axiosWithAuth"
 
-export const LOGIN_START = 'LOGIN_START'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILED = 'LOGIN_FAILED'
+const CREATE_USER_START = 'CREATE_USER_START'
+const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS'
+const CREATE_USER_FAILED = 'CREATE_USER_FAILED'
 
-export const GET_ACCOUNT_START = 'GET_DATA_START'
-export const GET_ACCOUNT_SUCCESS = 'GET_DATA_SUCCESS'
-export const GET_ACCOUNT_FAILED = 'GET_DATA_FAILED'
+const LOGIN_START = 'LOGIN_START'
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+const LOGIN_FAILED = 'LOGIN_FAILED'
 
-export const ADD_PLANT_START = 'ADD_PLANT_START'
-export const ADD_PLANT_SUCCESS = 'ADD_PLANT_SUCCESS'
-export const ADD_PLANT_FAILED = 'ADD_PLANT_FAILED'
+const GET_ACCOUNT_START = 'GET_DATA_START'
+const GET_ACCOUNT_SUCCESS = 'GET_DATA_SUCCESS'
+const GET_ACCOUNT_FAILED = 'GET_DATA_FAILED'
 
-export const GET_PLANT_START = 'GET_PLANT_START'
-export const GET_PLANT_SUCCESS = 'GET_PLANT_SUCCESS'
-export const GET_PLANT_FAILED = 'GET_PLANT_FAILED'
+const ADD_PLANT_START = 'ADD_PLANT_START'
+const ADD_PLANT_SUCCESS = 'ADD_PLANT_SUCCESS'
+const ADD_PLANT_FAILED = 'ADD_PLANT_FAILED'
 
-export const UPDATE_PLANT_START = 'UPDATE_PLANT_START'
-export const UPDATE_PLANT_SUCCESS = 'UPDATE_PLANT_SUCCESS'
-export const UPDATE_PLANT_FAILED = 'UPDATE_PLANT_FAILED'
+const GET_PLANT_START = 'GET_PLANT_START'
+const GET_PLANT_SUCCESS = 'GET_PLANT_SUCCESS'
+const GET_PLANT_FAILED = 'GET_PLANT_FAILED'
 
-export const DELETE_PLANT_START = 'DELETE_PLANT_START'
-export const DELETE_PLANT_SUCCESS = 'DELETE_PLANT_SUCCESS'
-export const DELETE_PLANT_FAILED = 'DELETE_PLANT_FAILED'
+const UPDATE_PLANT_START = 'UPDATE_PLANT_START'
+const UPDATE_PLANT_SUCCESS = 'UPDATE_PLANT_SUCCESS'
+const UPDATE_PLANT_FAILED = 'UPDATE_PLANT_FAILED'
 
-export const PLANT_ID ='PLANT_ID'
+const DELETE_PLANT_START = 'DELETE_PLANT_START'
+const DELETE_PLANT_SUCCESS = 'DELETE_PLANT_SUCCESS'
+const DELETE_PLANT_FAILED = 'DELETE_PLANT_FAILED'
+
+const PLANT_ID ='PLANT_ID'
+
+
+
+
+const createUser = (username, password) => {
+    return(dispatch) => {
+        dispatch({type: CREATE_USER_START})
+
+        return axios.post('', {username, password}) //insert end point for creating a new user account
+        .then((res) => {
+            localStorage.setItem('token', res.data.token)
+            dispatch({type: CREAT_USER_SUCCESS})
+        })
+        .catch((err) => {
+            const payload = err.response? err.response.data : err
+            dispatch({type: CREATE_USER_FAILED, payload})
+        })
+    }
+}
+
+
